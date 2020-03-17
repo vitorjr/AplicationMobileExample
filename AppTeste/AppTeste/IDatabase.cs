019ec44f551e484c752889229551364b8d418611
Joao Victor de A. Monteiro <joao.monteiro@fitbank.com.br>
2020-03-17 16:27:42 -0300

    Importando projeto para repositorio

---
+++ AppTeste/AppTeste/IDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace AppTeste
+{
+    public interface IDatabase
+    {
+        SQLiteConnection GetConnection();
+    }
+}
